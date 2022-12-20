1a8601db60a22f8af083480a8cae9c14504aa8d8
cskings14 <cskings14@gmail.com>
2022-12-20 01:20:51 -0500

    added more comments to helloworld

--- hello-world/helloworld.cpp
+++ hello-world/helloworld.cpp
@@ -1,7 +1,8 @@
-#include <iostream> 
-using namespace std;
+#include <iostream> // iostream is a c++ library that includes or has stream classes and methods. With this, ou can do thing like cin (to get an input) or cout (to print)
+using namespace std; // a namespace is basically a way to differentiate between variables. If two variables have the same name but are in different files, we should be able to distinguish them. 
 
-int main() {
+
+int main() { // main is the function with the return type of int
   // cout is the print statement unlike print in C. You then need 2 less than signs
   cout << "Hello World!";
   return 0;
