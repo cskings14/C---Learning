2935e4800767f9aa8e37e59606ae8b1023a834de
cskings14 <cskings14@gmail.com>
2022-12-20 01:05:32 -0500

    added comments to c++

--- hello-world/helloworld.cpp
+++ hello-world/helloworld.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 int main() {
+  // cout is the print statement unlike print in C. You then need 2 less than signs
   cout << "Hello World!";
   return 0;
 }
+// after a function is done, we then compile (just like C).
+// I can also type g++ -o runProgram helloworld.cpp
+// then just type ./runProgram to run the helloworld.cpp file (it is pretty much the same as C)
+
+
+// *Every time I change the file, I must recompile for the changes to be made*
+// Bash is easier to use than windows powershell just like C :(
