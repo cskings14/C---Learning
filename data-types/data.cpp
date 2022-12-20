a20365b9450e2d9462b6820407e3cc2a0ff3af68
cskings14 <cskings14@gmail.com>
2022-12-20 01:29:44 -0500

    added data.cpp

---
+++ data-types/data.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// yay, there are Strings. Did I mention that C is annoying
+int main(){
+    string s;
+    int num = 7;
+    cin >> s;
+    cout << s << num;
+    return 0;
+
+    
+
+   /* there are more data types like the following
+   int a = 7;
+   float b = 1.56;
+   bool c = false;
+   char d = 'a';
+   string e = "hello";
+   */
+
+}
